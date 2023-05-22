dd9f57a3f9829b4aed28cf4fb557834eba781848
efen stakes <efenstakes101@gmail.com>
2023-05-22 22:46:56 +0300

    add logo and update styles

--- app/components/appbar/component.tsx
+++ app/components/appbar/component.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 
-
 import './component.scss'
 import Link from 'next/link'
+import Image from 'next/image'
 
 type AppbarComponentProps = {
 
 }
 const AppbarComponent = () => {
+    
     return (
         <div className='appbar'>
 
-            <p className='appbar__text appbar__title'>
-                An Era
-            </p>
+            <div className='appbar__logo_container'>
+                <Image
+                    src={'/images/logo.jpg'}
+                    alt='Logo'
+                    className='appbar__logo_image'
+                    width={'40'}
+                    height={'40'}
+                />
+                <p className='appbar__text appbar__title'>
+                    An Era
+                </p>
+            </div>
             
             <p className='appbar__text appbar__bytext'>
                 <Link href={'https://felixportfolio-ea3f1.web.app'} target='_blank'>
